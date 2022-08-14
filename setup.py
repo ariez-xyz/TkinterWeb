9c650bd71c5bbbbb0047e5c0f49dea7e5bdd6d4f
ariez-xyz <dpape39@icloud.com>
2022-08-14 16:35:54 +0200

    add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="TkinterWeb",
+      version="1.0.0",
+      description="Fork of TkinterWeb for read",
+      license='MIT',
+      author="ariez_xyz",
+      author_email="",
+      url='https://github.com/ariez-xyz/TkinterWeb',
+      packages=['tkinterweb']
+)
